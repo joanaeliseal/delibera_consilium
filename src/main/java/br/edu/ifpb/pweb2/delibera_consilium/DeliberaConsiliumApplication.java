4b2436bedb356ba7e10d8dcefe14c2bfd65b6fc6
Joana Elise <joanaelise@gmail.com>
2025-11-11 20:44:27 -0300

    criado os packages MVC

---
+++ src/main/java/br/edu/ifpb/pweb2/delibera_consilium/DeliberaConsiliumApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.delibera_consilium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliberaConsiliumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliberaConsiliumApplication.class, args);
+	}
+
+}
