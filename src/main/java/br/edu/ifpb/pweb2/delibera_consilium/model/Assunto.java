b7c0c56ec7faedeeb28ec5de2df6f63a461b4de5
Felipe de Brito <felipebl1512@gmail.com>
2025-11-13 19:39:14 -0300

    feat: Cria classe Assunto

---
+++ src/main/java/br/edu/ifpb/pweb2/delibera_consilium/model/Assunto.java
@@ -0,0 +1,33 @@
+package br.edu.ifpb.pweb2.delibera_consilium.model;
+
+public class Assunto {
+    private int id;
+    private String nome;
+    
+    
+    public Assunto(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
