ee96bb25a71f6d312a63b93a64bf7651d15d460b
Felipe de Brito <felipebl1512@gmail.com>
2025-11-14 14:53:52 -0300

    feat: Cria classe Reuniao

---
+++ src/main/java/br/edu/ifpb/pweb2/delibera_consilium/model/Reuniao.java
@@ -0,0 +1,59 @@
+package br.edu.ifpb.pweb2.delibera_consilium.model;
+
+import java.util.Date;
+
+public class Reuniao {
+    private int id;
+    private Date dataReuniao;
+    private StatusReuniao status;
+    private byte ata;
+    
+    
+    public Reuniao(int id, Date dataReuniao, StatusReuniao status, byte ata) {
+        this.id = id;
+        this.dataReuniao = dataReuniao;
+        this.status = status;
+        this.ata = ata;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public Date getDataReuniao() {
+        return dataReuniao;
+    }
+
+
+    public void setDataReuniao(Date dataReuniao) {
+        this.dataReuniao = dataReuniao;
+    }
+
+
+    public StatusReuniao getStatus() {
+        return status;
+    }
+
+
+    public void setStatus(StatusReuniao status) {
+        this.status = status;
+    }
+
+
+    public byte getAta() {
+        return ata;
+    }
+
+
+    public void setAta(byte ata) {
+        this.ata = ata;
+    }
+
+}
