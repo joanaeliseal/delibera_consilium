acdfb3cd4a5d45bb00fb4005b3220d931ffc99c0
Felipe de Brito <felipebl1512@gmail.com>
2025-11-30 16:03:15 -0300

    feat: Atualiza classe TipoDecisao

--- src/main/java/br/edu/ifpb/pweb2/delibera_consilium/model/TipoDecisao.java
+++ src/main/java/br/edu/ifpb/pweb2/delibera_consilium/model/TipoDecisao.java
@@ -1,19 +1,6 @@
 package br.edu.ifpb.pweb2.delibera_consilium.model;
 
 public enum TipoDecisao {
-
-    DEFERIMENTO(1),
-    INDEFERIMENTO(0); // Geralmente 1 para 'sim/positivo' e 0 para 'nao/negativo'
-
-    // 2. Campo privado e final para armazenar o valor int
-    private final int valor;
-
-    private TipoDecisao(int valor) {
-        this.valor = valor;
-    }
-
-    public int getValor() {
-        return valor;
-    }
-
+    DEFERIMENTO,
+    INDEFERIMENTO
 }
