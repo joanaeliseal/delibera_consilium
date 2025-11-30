1a75dbc1414c16bf5256f42a216a62bbb515cf4c
Felipe de Brito <felipebl1512@gmail.com>
2025-11-30 16:02:21 -0300

    feat: Atualiza classe StatusReuniao

--- src/main/java/br/edu/ifpb/pweb2/delibera_consilium/model/StatusReuniao.java
+++ src/main/java/br/edu/ifpb/pweb2/delibera_consilium/model/StatusReuniao.java
@@ -1,17 +1,6 @@
 package br.edu.ifpb.pweb2.delibera_consilium.model;
 
 public enum StatusReuniao {
-    ENCERRADA(1),
-    PROGRAMADA(0);
-
-    private final int valor;
-    
-    private StatusReuniao(int valor){
-        this.valor = valor;
-    } 
-
-    public int getValor(){
-        return valor;
-    }
-
+    PROGRAMADA,
+    ENCERRADA
 }
