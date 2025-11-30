ae85a7c01546714c3fcb44365ce126c5a100cc39
Felipe de Brito <felipebl1512@gmail.com>
2025-11-30 16:04:23 -0300

    feat: Atualiza classe TipoVoto

--- src/main/java/br/edu/ifpb/pweb2/delibera_consilium/model/TipoVoto.java
+++ src/main/java/br/edu/ifpb/pweb2/delibera_consilium/model/TipoVoto.java
@@ -1,17 +1,6 @@
 package br.edu.ifpb.pweb2.delibera_consilium.model;
 
 public enum TipoVoto {
-    COM_RELATOR(0),
-    DIVERGENTE(1);
-
-    private final int valor;
-
-    private TipoVoto(int valor){
-        this.valor = valor;
-    }
-
-    public int getValor(){
-        return valor;
-    }
-
+    COM_RELATOR,
+    DIVERGENTE
 }
