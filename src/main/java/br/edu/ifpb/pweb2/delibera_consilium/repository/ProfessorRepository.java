1f81676867d5c92d01abae5bac26c6ceeac74b6d
Felipe de Brito <felipebl1512@gmail.com>
2025-11-30 17:33:32 -0300

    feat: Cria ProfessorRepository

---
+++ src/main/java/br/edu/ifpb/pweb2/delibera_consilium/repository/ProfessorRepository.java
@@ -0,0 +1,12 @@
+package br.edu.ifpb.pweb2.delibera_consilium.repository;
+
+import br.edu.ifpb.pweb2.delibera_consilium.model.Professor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ProfessorRepository extends JpaRepository<Professor, Long> {
+    //Buscar apenas professores que são coordenadores
+    List<Professor> findByCoordenadorTrue();
+}
