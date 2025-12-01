a2a0eff155c57cd4a136c5cb816bafcf6c17a255
Felipe de Brito <felipebl1512@gmail.com>
2025-12-01 16:34:12 -0300

    feat: Atualiza controller ProcessoProfessor

--- src/main/java/br/edu/ifpb/pweb2/delibera_consilium/controller/ProcessoProfessorController.java
+++ src/main/java/br/edu/ifpb/pweb2/delibera_consilium/controller/ProcessoProfessorController.java
@@ -1,24 +1,37 @@
 package br.edu.ifpb.pweb2.delibera_consilium.controller;
 
+import br.edu.ifpb.pweb2.delibera_consilium.model.Professor;
 import br.edu.ifpb.pweb2.delibera_consilium.service.ProcessoService;
+import br.edu.ifpb.pweb2.delibera_consilium.service.ProfessorService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/professor/processos")
 public class ProcessoProfessorController {
 
-    private final ProcessoService service;
+    private final ProcessoService processoService;
+    private final ProfessorService professorService; 
 
-    public ProcessoProfessorController(ProcessoService service) {
-        this.service = service;
+    public ProcessoProfessorController(ProcessoService processoService, ProfessorService professorService) {
+        this.processoService = processoService;
+        this.professorService = professorService;
     }
 
     @GetMapping
     public String listarAtribuidos(Model model) {
-        model.addAttribute("processos", service.listarTodos());
+        Professor professorLogado = this.getProfessorLogado();
+
+        // REQFUNC 3
+        model.addAttribute("processos", processoService.listarPorRelator(professorLogado));
+        
         return "professor/processo/list";
+    }
+
+    // metodo fake pra simular login 
+    private Professor getProfessorLogado() {
+        return professorService.buscarPorId(1L); 
     }
 }
