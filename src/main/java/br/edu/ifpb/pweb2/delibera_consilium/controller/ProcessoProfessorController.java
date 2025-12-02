e3ab55fb785a95907a75b51437d50b2f2390d93f
Joana Elise <joanaelise@gmail.com>
2025-12-02 19:50:21 -0300

    feat: atualiza controller processo-professor

--- src/main/java/br/edu/ifpb/pweb2/delibera_consilium/controller/ProcessoProfessorController.java
+++ src/main/java/br/edu/ifpb/pweb2/delibera_consilium/controller/ProcessoProfessorController.java
@@ -7,13 +7,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/professor/processos")
 public class ProcessoProfessorController {
 
     private final ProcessoService processoService;
-    private final ProfessorService professorService; 
+    private final ProfessorService professorService;
 
     public ProcessoProfessorController(ProcessoService processoService, ProfessorService professorService) {
         this.processoService = processoService;
@@ -21,17 +22,19 @@ public class ProcessoProfessorController {
     }
 
     @GetMapping
-    public String listarAtribuidos(Model model) {
-        Professor professorLogado = this.getProfessorLogado();
+    // O param usuario é opcional, se não for informado na URL, ele assume que o id é 1
+    public String listarAtribuidos(@RequestParam(required = false, defaultValue = "1") Long usuario, Model model) {
+        
+        Professor professorLogado = professorService.buscarPorId(usuario);
 
-        // REQFUNC 3
-        model.addAttribute("processos", processoService.listarPorRelator(professorLogado));
+        if (professorLogado == null) {
+            model.addAttribute("processos", null);
+        } else {
+            // REQFUNC 3: busca apenas processos onde ele é o relator
+            model.addAttribute("processos", processoService.listarPorRelator(professorLogado));
+            model.addAttribute("professorLogado", professorLogado); 
+        }
         
         return "professor/processo/list";
-    }
-
-    // metodo fake pra simular login 
-    private Professor getProfessorLogado() {
-        return professorService.buscarPorId(1L); 
     }
 }
