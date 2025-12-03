f2a0709743b09e6eaf84e92bc1ac0639926908b4
Felipe de Brito <felipebl1512@gmail.com>
2025-12-03 02:12:54 -0300

    feat: Adiciona validação nos campos necessários

--- src/main/java/br/edu/ifpb/pweb2/delibera_consilium/model/Professor.java
+++ src/main/java/br/edu/ifpb/pweb2/delibera_consilium/model/Professor.java
@@ -1,9 +1,9 @@
 package br.edu.ifpb.pweb2.delibera_consilium.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank; 
 import lombok.Data;
 import lombok.ToString;
-
 import java.util.List;
 
 @Data
@@ -13,19 +13,26 @@ public class Professor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "O nome é obrigatório")
     private String nome;
+
     private String fone;
+
+    @NotBlank(message = "A matrícula é obrigatória")
     private String matricula;
+
+    @NotBlank(message = "O login é obrigatório")
     private String login;
+
+    @NotBlank(message = "A senha é obrigatória")
     private String senha;
+    
     private boolean coordenador;
 
-    // Processos onde este professor é o Relator
     @OneToMany(mappedBy = "relator")
     @ToString.Exclude
     private List<Processo> processosRelator;
 
-    // Colegiados dos quais ele é membro
     @ManyToMany(mappedBy = "membros")
     @ToString.Exclude
     private List<Colegiado> colegiados;
